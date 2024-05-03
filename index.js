43df25415aafc25e9a9a267ad907a8d91af5a526
prameela <prameelarpujari>
2024-05-03 15:25:07 +0530

    Complted the random select project.

---
+++ index.js
@@ -0,0 +1,108 @@
+// const tagsElements = document.getElementById("tags");
+// const textArea = document.getElementById("textArea");
+
+// const creatTags = (input) => {
+//     const tags = input
+//         .split(",")
+//         .filter((tag) => 
+//             tag.trim() !== "")
+//         .map((tag) => tag.trim());
+
+//     tagsElements.innerHTML = "";
+
+//     tags.forEach((tag) => {
+//         const tagElement = document.createElement("span");
+//         tagElement.classList.add("tag");
+//         tagElement.textContent = tag;
+//         tagsElements.appendChild(tagElement);
+//     });
+// };
+
+// const pickRandomTag = () => {
+//     const tags = document.querySelectorAll(".tag");
+//     return tags[Math.floor(Math.random() * tags.length)];
+// };
+
+// const highlightTag = (tag) => tag.classList.add("highlight");
+// const unHighlightTag = (tag) => tag.classList.remove("highlight");
+
+// const randomSelect = () => {
+//     const times = 20;
+//     const interval = setInterval(() => {
+//         const randomTag = pickRandomTag();
+//         highlightTag(randomTag);
+//         setTimeout(() => {
+//             unHighlightTag(randomTag);
+//         }, 100);
+//     }, 100);
+
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         setTimeout(() => {
+//             const randomTag = pickRandomTag();
+//             highlightTag(randomTag);
+//         }, 100);
+//     }, times * 100);
+// };
+
+// textArea.addEventLister("keyup", (e) => {
+//     creatTags(e.target.value);
+//     if (e.key === "Enter") {
+//         setTimeout(() => (e.target.value = ""), 10);
+//         randomSelect();
+//     }
+// });
+
+const tagsElements = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+const createTags = (input) => {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsElements.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagElement = document.createElement("span");
+    tagElement.classList.add("tag");
+    tagElement.innerText = tag;
+    tagsElements.appendChild(tagElement);
+  });
+};
+
+const pickRandomTag = () => {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+};
+
+const highlightTag = (tag) => tag.classList.add("highlight");
+const unHighlightTag = (tag) => tag.classList.remove("highlight");
+
+const randomSelect = () => {
+  const times = 20;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+};
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if (e.key === "Enter") {
+    setTimeout(() => (e.target.value = ""), 10);
+    randomSelect();
+  }
+});
